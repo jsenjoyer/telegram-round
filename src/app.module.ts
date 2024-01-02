ca27ca53fc48de0d2342d76b1942fe7f6af8471a
jsenjoyer <jsenjoyer@icloud.com>
2024-01-02 22:45:46 +0100

    telegraf

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TelegrafModule } from 'nestjs-telegraf';
+import * as process from 'process';
+import { Update } from 'nestjs-telegraf';
+import { AppUpdate } from './app.update';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    TelegrafModule.forRoot({
+      token: process.env.TELEGRAM_BOT_KEY,
+    }),
+  ],
+  controllers: [],
+  providers: [AppUpdate],
 })
 export class AppModule {}
---
+++ src/app.update.ts
@@ -0,0 +1,26 @@
+import { Update, Ctx, Start, Help, On, Hears } from 'nestjs-telegraf';
+
+@Update()
+export class AppUpdate {
+  @Start()
+  async start(@Ctx() ctx: any) {
+    console.log('start');
+    await ctx.reply('Welcome');
+  }
+
+  @Help()
+  async help(@Ctx() ctx: any) {
+    await ctx.reply('Send me a sticker');
+  }
+
+  @On('sticker')
+  async on(@Ctx() ctx: any) {
+    await ctx.reply('👍');
+  }
+
+  @Hears('hi')
+  async hears(@Ctx() ctx: any) {
+    console.log(ctx);
+    await ctx.reply('Hey there');
+  }
+}
