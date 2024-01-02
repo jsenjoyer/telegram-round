72fcd5847e318445f8b027b9c426e59fe6c6491f
jsenjoyer <jsenjoyer@icloud.com>
2024-01-03 00:14:16 +0100

    enable user bd

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { TelegrafModule } from 'nestjs-telegraf';
+import { TelegrafModule, Use } from 'nestjs-telegraf';
 import * as process from 'process';
 import { AppUpdate } from './app.update';
 import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './schemas/user.schema';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { MongooseModule } from '@nestjs/mongoose';
       token: process.env.TELEGRAM_BOT_KEY,
     }),
     MongooseModule.forRoot(process.env.MONGO_DB),
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
   controllers: [],
   providers: [AppUpdate],
--- src/app.update.ts
+++ src/app.update.ts
@@ -1,11 +1,29 @@
 import { Update, Ctx, Start, Help, On, Hears } from 'nestjs-telegraf';
 import { Context } from 'telegraf';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './schemas/user.schema';
+import { Model } from 'mongoose';
 
 @Update()
 export class AppUpdate {
+  constructor(
+    @InjectModel(User.name) private readonly userModel: Model<User>,
+  ) {}
+
   @Start()
   async start(@Ctx() ctx: Context) {
-    await ctx.reply('welcome');
+    const { id, first_name, username } = ctx.message.from;
+    const user = await this.userModel.findOne({ tgId: id });
+    if (!user) {
+      const newUser = new this.userModel({
+        firstName: first_name,
+        userName: username,
+        tgId: id,
+      });
+      await newUser.save();
+      await ctx.reply('welcome');
+    }
+    await ctx.reply('u already exist');
   }
 
   @Hears('hi')
