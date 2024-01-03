8165ffc96f7177a4a5b5181158643af8474048ec
jsenjoyer <jsenjoyer@icloud.com>
2024-01-03 08:42:01 +0100

    user service init

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from '../../schemas/user/user.schema';
+import { UserService } from './user.service';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
+  providers: [UserService],
 })
 export class UserModule {}
