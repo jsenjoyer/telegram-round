e9f1aaf7066db992d4700b13607d49fc9ae2d58d
jsenjoyer <jsenjoyer@icloud.com>
2024-01-04 01:43:52 +0100

    change step to video after /start

--- src/modules/user/user.update.ts
+++ src/modules/user/user.update.ts
@@ -1,14 +1,17 @@
 import { Start, Update } from 'nestjs-telegraf';
 import { Context } from 'telegraf';
 import { UserService } from './user.service';
+import { SceneContext } from 'telegraf/typings/scenes';
+import { VIDEO_WIZARD } from '../video/video.wizard';
 
 @Update()
 export class UserUpdate {
   constructor(private readonly userService: UserService) {}
 
   @Start()
-  async start(ctx: Context) {
+  async start(ctx: SceneContext) {
     const { first_name: firstName, id, username: userName } = ctx.from;
+    await ctx.scene.enter(VIDEO_WIZARD);
     await this.userService.registerUser({
       firstName,
       userName,
