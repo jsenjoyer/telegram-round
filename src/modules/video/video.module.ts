506222c7c0f97f26964f6a861315bd46cd8d9011
jsenjoyer <jsenjoyer@icloud.com>
2024-01-04 05:57:26 +0100

    change build of the ffmpeg

--- src/modules/video/video.module.ts
+++ src/modules/video/video.module.ts
@@ -1,13 +1,24 @@
-import { Module } from '@nestjs/common';
+import { Module, OnModuleInit } from '@nestjs/common';
 import { VideoService } from './video.service';
 import { VideoUpdate } from './video.update';
 import { VideoWizard } from './video.wizard';
 import { SharedModule } from '../shared/shared.module';
-import { FluentFfmpegModule } from '@mrkwskiti/fluent-ffmpeg-nestjs';
+import {
+  Ffmpeg,
+  FluentFfmpegModule,
+  InjectFluentFfmpeg,
+} from '@mrkwskiti/fluent-ffmpeg-nestjs';
+import * as ffmpegPath from 'ffmpeg-static';
 
 @Module({
   imports: [SharedModule, FluentFfmpegModule.forRoot()],
   providers: [VideoService, VideoUpdate, VideoWizard],
   exports: [VideoWizard],
 })
-export class VideoModule {}
+export class VideoModule implements OnModuleInit {
+  constructor(@InjectFluentFfmpeg() private readonly ffmpeg: Ffmpeg) {}
+
+  onModuleInit() {
+    this.ffmpeg.setFfmpegPath(ffmpegPath as unknown as string);
+  }
+}
