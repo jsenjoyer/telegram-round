c47105c0174fe5f9ca8437038acae39cf06a4e18
jsenjoyer <jsenjoyer@icloud.com>
2024-01-04 01:41:58 +0100

    shared module init

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -3,10 +3,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from '../../schemas/user/user.schema';
 import { UserService } from './user.service';
 import { UserUpdate } from './user.update';
+import { SharedModule } from '../shared/shared.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    SharedModule,
   ],
   providers: [UserService, UserUpdate],
 })
