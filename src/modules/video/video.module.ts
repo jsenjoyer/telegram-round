7e71d7980b2b2c85583507ee56af9810e2d8f6bb
jsenjoyer <jsenjoyer@icloud.com>
2024-01-04 01:43:27 +0100

    video module init

--- src/modules/video/video.module.ts
+++ src/modules/video/video.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { VideoService } from './video.service';
+import { VideoUpdate } from './video.update';
+import { VideoWizard } from './video.wizard';
+import { SharedModule } from '../shared/shared.module';
 
-@Module({})
+@Module({
+  imports: [SharedModule],
+  providers: [VideoService, VideoUpdate, VideoWizard],
+  exports: [VideoWizard],
+})
 export class VideoModule {}
