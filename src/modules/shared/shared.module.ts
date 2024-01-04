dce772665c505aadbc7984e4ea248bfc63d4e48e
jsenjoyer <jsenjoyer@icloud.com>
2024-01-04 01:41:51 +0100

    http module init

--- src/modules/shared/shared.module.ts
+++ src/modules/shared/shared.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { FilesService } from '../../services/files.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+  imports: [HttpModule],
   providers: [FilesService],
+  exports: [HttpModule, FilesService],
 })
 export class SharedModule {}
