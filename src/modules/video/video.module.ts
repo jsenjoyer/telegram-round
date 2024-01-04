99ce9c83aca81b9d464dcfcce2ba6f4c59743ab1
jsenjoyer <jsenjoyer@icloud.com>
2024-01-04 03:27:58 +0100

    ffmpeg libs init

--- src/modules/video/video.module.ts
+++ src/modules/video/video.module.ts
@@ -3,9 +3,10 @@ import { VideoService } from './video.service';
 import { VideoUpdate } from './video.update';
 import { VideoWizard } from './video.wizard';
 import { SharedModule } from '../shared/shared.module';
+import { FluentFfmpegModule } from '@mrkwskiti/fluent-ffmpeg-nestjs';
 
 @Module({
-  imports: [SharedModule],
+  imports: [SharedModule, FluentFfmpegModule.forRoot()],
   providers: [VideoService, VideoUpdate, VideoWizard],
   exports: [VideoWizard],
 })
