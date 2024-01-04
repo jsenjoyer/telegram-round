135eb382deac5c0b2d8aa0157c4c71110ea428e4
jsenjoyer <jsenjoyer@icloud.com>
2024-01-04 03:27:55 +0100

    ffmpeg libs init

--- src/modules/shared/shared.module.ts
+++ src/modules/shared/shared.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { FilesService } from '../../services/files.service';
 import { HttpModule } from '@nestjs/axios';
+import { FluentFfmpegModule } from '@mrkwskiti/fluent-ffmpeg-nestjs';
 
 @Module({
-  imports: [HttpModule],
+  imports: [HttpModule, FluentFfmpegModule.forRoot()],
   providers: [FilesService],
   exports: [HttpModule, FilesService],
 })
